53afb51b53b7635d7777a05e512c6e22fb7b3408
John Daniels <johnsdaniels@gmail.com>
2014-03-23 00:33:38 -0400

    Add dependency on flask-sqlalchemy

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['flask>=0.6'],
+    install_requires = [
+        'flask>=0.6',
+        'Flask-SQLAlchemy>=1.0'
+    ],
 
     # could also include long_description, download_url, classifiers, etc.
 )
