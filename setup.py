c6435afe33c139c8292af290a89539652eb8243c
John Daniels <johnsdaniels@gmail.com>
2014-03-23 00:19:57 -0400

    setup better package structure for installing in virtualenv

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "SBCS Site",
+    version = "0.1",
+    packages = find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['flask>=0.6'],
+
+    # could also include long_description, download_url, classifiers, etc.
+)
